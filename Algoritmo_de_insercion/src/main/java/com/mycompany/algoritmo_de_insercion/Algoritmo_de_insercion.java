e014359921f8f02c8df49e8dfb45a9194877e925
Johann3421 <1355430@senati.pe>
2023-08-24 14:41:47 -0500

    Creando el algoimo de insercion, iniciando con el scanner

--- Algoritmo_de_insercion/src/main/java/com/mycompany/algoritmo_de_insercion/Algoritmo_de_insercion.java
+++ Algoritmo_de_insercion/src/main/java/com/mycompany/algoritmo_de_insercion/Algoritmo_de_insercion.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.algoritmo_de_insercion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alumno
@@ -11,29 +13,7 @@ package com.mycompany.algoritmo_de_insercion;
 public class Algoritmo_de_insercion {
 
     public static void main(String[] args) {
-         int[] arreglo = {64, 34, 25, 12, 22, 11, 90};
-        ordenarPorInsercion(arreglo);
+        Scanner sc = new Scanner(System.in);
         
-        System.out.println("Arreglo ordenado:");
-        for (int elemento : arreglo) {
-            System.out.print(elemento + " ");
-        }
-    }
-
-    public static void ordenarPorInsercion(int[] arr) {
-        int n = arr.length;
-        
-        for (int i = 1; i < n; i++) {
-            int clave = arr[i];
-            int j = i - 1;
-            
-            // Mover elementos del arreglo[0..i-1] que son mayores que la clave
-            // a una posición adelante de su posición actual
-            while (j >= 0 && arr[j] > clave) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = clave;
-        }
     }
 }
