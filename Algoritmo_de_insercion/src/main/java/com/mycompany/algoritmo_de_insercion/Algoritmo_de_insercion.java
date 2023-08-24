6d50bd7a58bf6ed9cce687c713cda87b7ca9a570
Johann3421 <1355430@senati.pe>
2023-08-24 16:54:54 -0500

    Agregando comentarios y ademas de corregir variables

--- Algoritmo_de_insercion/src/main/java/com/mycompany/algoritmo_de_insercion/Algoritmo_de_insercion.java
+++ Algoritmo_de_insercion/src/main/java/com/mycompany/algoritmo_de_insercion/Algoritmo_de_insercion.java
@@ -13,36 +13,44 @@ import javax.swing.JOptionPane;
 public class Algoritmo_de_insercion {
 
     public static void main(String[] args) {
+        /*Creamos una varable para el scanner y ademas debemos crear una variable llamada numero de elementos
+        que pregunte al usuario la cantidad de elementos pero con una ventana emergente */
         Scanner sc = new Scanner(System.in);
-        int arr[],nElementos,pos,aux;
+        int arreglo[],nElementos,pos,aux;
         
         nElementos = Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de elementos del arreglo: "));
-        arr = new  int [nElementos];
+        arreglo = new  int [nElementos];
+        /*Asignar cuantos elementos puede tener la variable nElementos */
+        
         
         System.out.println("Digite el arreglo: ");
         for (int i=0;i<nElementos;i++){
         System.out.print((i+1)+".Digite un numero: ");
-        arr[i]=sc.nextInt();
-        
+        arreglo[i]=sc.nextInt();
+        /*Utilizamos un bucle for para que i=0 hasta que sea menor al numero de elementos e iterar a traves de una secuencia de numeros 
+         de una forma concisa para aumentar el valor de una variable en 1 en cada paso de un bucle*/
     }
         //ordenamiento por insercion
+        /* añadir un itinerario para guiarnos a traves de +1 */
         for (int i = 0;i<nElementos;i++){
             pos = i;
-            aux=arr[i];
-            
-            while((pos>0)&&(arr[pos-1] > aux)){
-                arr[pos]=arr[pos-1];
+            aux=arreglo[i];
+        //añadimos 2 variables llamadas pos y aux, que sirven para ordenar y ademas para asignar el valor dentro del elemento    
+            while((pos>0)&&(arreglo[pos-1] > aux)){
+                arreglo[pos]=arreglo[pos-1];
                 pos--;
             }
-            arr[pos]=aux;
+            arreglo[pos]=aux;
         }
+        //añadir un bucle while para comprobar las posiciones de los numeros menos la posicion 0 y que el iterador debe ser mayor que el numero actual
+        
         System.out.print("Orden ascendente: ");
         for (int i = 0; i < nElementos; i++) {
-            System.out.print(arr[i]+" - ");
+            System.out.print(arreglo[i]+" - ");
         }
         System.out.print("\nOrden descendente: ");
         for (int i =(nElementos-1); i>=0;i--) {
-            System.out.print(arr[i]+" - ");
+            System.out.print(arreglo[i]+" - ");
         }
         System.out.println("");
     }
