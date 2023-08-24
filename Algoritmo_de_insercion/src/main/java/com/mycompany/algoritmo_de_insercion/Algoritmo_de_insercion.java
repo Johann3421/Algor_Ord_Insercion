fcb77906cbfab5e7b1d8178cf26d3a6f762fa7f6
Johann3421 <1355430@senati.pe>
2023-08-24 15:01:01 -0500

    Agregando tda la funcion for para ordenar de manera ascendete y descendente

--- Algoritmo_de_insercion/src/main/java/com/mycompany/algoritmo_de_insercion/Algoritmo_de_insercion.java
+++ Algoritmo_de_insercion/src/main/java/com/mycompany/algoritmo_de_insercion/Algoritmo_de_insercion.java
@@ -14,17 +14,36 @@ public class Algoritmo_de_insercion {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[],nElementos;
+        int arr[],nElementos,pos,aux;
         
         nElementos = Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de elementos del arreglo: "));
         arr = new  int [nElementos];
         
         System.out.println("Digite el arreglo: ");
         for (int i=0;i<nElementos;i++){
-        System.out.println((i+1)+".Digite un numero: ");
+        System.out.print((i+1)+".Digite un numero: ");
         arr[i]=sc.nextInt();
         
     }
         //ordenamiento por insercion
+        for (int i = 0;i<nElementos;i++){
+            pos = i;
+            aux=arr[i];
+            
+            while((pos>0)&&(arr[pos-1] > aux)){
+                arr[pos]=arr[pos-1];
+                pos--;
+            }
+            arr[pos]=aux;
+        }
+        System.out.print("Orden ascendente: ");
+        for (int i = 0; i < nElementos; i++) {
+            System.out.print(arr[i]+" - ");
+        }
+        System.out.print("Orden descendente: ");
+        for (int i =(nElementos-1); i>=0;i--) {
+            System.out.print(arr[i]+" - ");
+        }
+        System.out.println("");
     }
 }
