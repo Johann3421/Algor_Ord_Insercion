189719803b1c1a829fc8cfda5a99e2e3c6ef83b8
Johann3421 <1355430@senati.pe>
2023-08-24 14:52:45 -0500

    Corriginedo algunas fallas

--- Algoritmo_de_insercion/src/main/java/com/mycompany/algoritmo_de_insercion/Algoritmo_de_insercion.java
+++ Algoritmo_de_insercion/src/main/java/com/mycompany/algoritmo_de_insercion/Algoritmo_de_insercion.java
@@ -17,7 +17,14 @@ public class Algoritmo_de_insercion {
         int arr[],nElementos;
         
         nElementos = Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de elementos del arreglo: "));
+        arr = new  int [nElementos];
         
+        System.out.println("Digite el arreglo: ");
+        for (int i=0;i<nElementos;i++){
+        System.out.println((i+1)+".Digite un numero: ");
+        arr[i]=sc.nextInt();
         
+    }
+        //ordenamiento por insercion
     }
 }
